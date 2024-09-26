cf179b3aa2b4444b80b310d1892c09b9fde9afd7
Klee Collage <klee.collage@gmail.com>
2024-09-25 23:45:40 -0500

    Operations implemented

--- src/presentation/hooks/useCalculator.ts
+++ src/presentation/hooks/useCalculator.ts
@@ -1,8 +1,40 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
+
+enum Operator {
+  add,
+  substract,
+  multiply,
+  divide,
+}
 
 export const useCalculator = () => {
 
   const [number, setNumber] = useState('0')
+  const [prevNumber, setPrevNumber] = useState('0')
+
+  const lastOperation = useRef<Operator>();
+
+  const clean = () => {
+    setNumber('0');
+    setPrevNumber('0');
+  }
+
+  const deleteOperation = () => {
+    if(number.length == 1 || number.startsWith('0') ) return setNumber('0')
+    if(number.startsWith('-') && number.length == 2) return setNumber('0')
+    
+    return setNumber(number.slice(0,-1))
+  }
+
+  const toggleSign = () => {
+    if(number.includes('-')) {
+      return setNumber( number.replace('-', ''))
+    }
+    if(number.startsWith('-')){
+      return setNumber('-' + number);
+    }
+    return setNumber('-' + number);
+  }
 
   const buildNumber = (numberString: string) => {
     if(number.includes('.') && numberString === '.') return;
@@ -30,10 +62,77 @@ export const useCalculator = () => {
 
   }
 
+  const setLastNumber = () => {
+    if(number.endsWith('.')) {
+      setPrevNumber(number.slice(0,-1));
+    } else {
+      setPrevNumber(number);
+    }
+    setNumber('0');
+  }
+
+  const divideOperation = () => {
+    setLastNumber();
+    lastOperation.current = Operator.divide
+  }
+
+  const multiplyOperation = () => {
+    setLastNumber();
+    lastOperation.current = Operator.multiply
+  }
+
+  const substractOperation = () => {
+    setLastNumber();
+    lastOperation.current = Operator.substract
+  }
+
+  const addOperation = () => {
+    setLastNumber();
+    lastOperation.current = Operator.add
+  }
+
+  const calculateResult = () => {
+    const num1 = Number(number);
+    const num2 = Number(prevNumber);
+
+    switch(lastOperation.current) {
+
+      case Operator.add:
+        setNumber(`${ num1 + num2}`)
+        break;
+
+      case Operator.substract:
+        setNumber(`${ num2 - num1}`)
+        break;
+
+      case Operator.divide:
+        setNumber(`${ num2 / num1}`)
+        break;
+
+      case Operator.multiply:
+        setNumber(`${ num1 * num2}`)
+        break;
+
+      default:
+        throw new Error("Operation not implemented");
+        
+    }
+    setPrevNumber('0');
+  }
+
   return{
-    // porperties
+    // properties
     number,
+    prevNumber,
     // methods
-    buildNumber
+    buildNumber,
+    toggleSign,
+    clean,
+    deleteOperation,
+    divideOperation,
+    multiplyOperation,
+    substractOperation,
+    addOperation,
+    calculateResult
   }
 }
--- src/presentation/screens/CalculatorScreen.tsx
+++ src/presentation/screens/CalculatorScreen.tsx
@@ -5,46 +5,53 @@ import { CalculatorButton } from '../components/CalculatorButton' ;
 import { useCalculator } from '../hooks/useCalculator';
 
 export const CalculatorScreen = () => {
-  const { number, buildNumber} = useCalculator()
+  const { 
+    number, prevNumber,
+    buildNumber, toggleSign, clean, deleteOperation, calculateResult,
+    divideOperation, multiplyOperation, substractOperation, addOperation
+  } = useCalculator()
+
   return (
     <View style={styles.calculatorContainer}>
       <View style={{paddingHorizontal:30, paddingBottom:20}}>
-        <Text style={styles.mainResult} adjustsFontSizeToFit numberOfLines={1}> { number } </Text>
-        <Text style={styles.subResult}>15</Text>
+        <Text style={styles.mainResult} adjustsFontSizeToFit numberOfLines={1}>{ number } </Text>
+        <Text style={styles.subResult} adjustsFontSizeToFit numberOfLines={1}>
+          { (prevNumber === '0') ? ' ' : prevNumber }
+        </Text>
       </View>
 
       <View style={styles.row}>
-         <CalculatorButton onPress={ () => console.log('C') } blackText color={colors.lightGray} label= 'C' />
-         <CalculatorButton onPress={ () => console.log('+/-' ) } blackText color={colors.lightGray} label= '+/-' />
-         <CalculatorButton onPress={ () => console.log('del') } blackText color={colors.lightGray} label= 'del' />
-         <CalculatorButton onPress={ () => console.log('÷') } color={colors.orange} label= '÷' />         
+         <CalculatorButton onPress={ () => clean() } blackText color={colors.lightGray} label= 'C' />
+         <CalculatorButton onPress={ toggleSign } blackText color={colors.lightGray} label= '+/-' />
+         <CalculatorButton onPress={ deleteOperation } blackText color={colors.lightGray} label= 'del' />
+         <CalculatorButton onPress={ divideOperation } color={colors.orange} label= '÷' />         
       </View>
 
       <View style={styles.row}>
         <CalculatorButton onPress={ () => buildNumber('7') } label='7' />
         <CalculatorButton onPress={ () => buildNumber('8') } label='8' />
         <CalculatorButton onPress={ () => buildNumber('9') } label='9' />
-        <CalculatorButton onPress={ () => console.log('x') } color={colors.orange} label='x' />
+        <CalculatorButton onPress={ multiplyOperation } color={colors.orange} label='x' />
       </View>
 
       <View style={styles.row}>
         <CalculatorButton onPress={ () => buildNumber('4') } label='4' />
         <CalculatorButton onPress={ () => buildNumber('5') } label='5' />
         <CalculatorButton onPress={ () => buildNumber('6') } label='6' />
-        <CalculatorButton onPress={ () => console.log('-') } color={colors.orange} label='-' />
+        <CalculatorButton onPress={ substractOperation } color={colors.orange} label='-' />
       </View>
 
       <View style={styles.row}>
         <CalculatorButton onPress={ () => buildNumber('1') } label='1' />
         <CalculatorButton onPress={ () => buildNumber('2') } label='2' />
         <CalculatorButton onPress={ () => buildNumber('3') } label='3' />
-        <CalculatorButton onPress={ () => console.log('+') } color={colors.orange} label='+' />
+        <CalculatorButton onPress={ addOperation } color={colors.orange} label='+' />
       </View>
 
       <View style={styles.row}>
         <CalculatorButton onPress={ () => buildNumber('0') } label='0' doubleSize />
         <CalculatorButton onPress={ () => buildNumber('.') } label='.' />
-        <CalculatorButton onPress={ () => console.log('=') } color={colors.orange} label='=' />
+        <CalculatorButton onPress={ calculateResult } color={colors.orange} label='=' />
       </View>
 
     </View>
